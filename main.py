9e5b7b4ffdc11fdb5aed0adfbd9dfea48d486f5e
SeananTQ <38140149+SeananTQ@users.noreply.github.com>
2018-04-20 00:59:31 +0800

    大功告成
    完善了BBQ服务器所需的基本功能
    1、校对时间
    2、上传分数
    3、查询排行榜

--- main.py
+++ main.py
@@ -29,44 +29,6 @@ def theTime():
 @app.route("/bbq/<int:model_type>/<int:getId>/<int:getCount>", methods = ['GET', 'POST'])
 def bbqRoute(model_type,getId,getCount):
 	resultMsg = ""
-	# if request.method == 'POST':
-	#     playerId = int( request.form.get('pid'))
-	#     playerName = request.form.get('pname')
-	#     newScore = int(request.form.get('score'))
-	#     password = request.form.get('passwd')
-	#     # 首先验证数据合法性
-	#     if password:
-	#         print('password is :%s' % password)
-	#         # 查询该角色是否存在
-	#         tempStr =str( PlayerScore.exist('_id', playerId))
-	#         if tempStr == "None":
-	#             print('所查询角色%s不存在' % playerId)
-	#             # 如果不存在则新增
-	#             playerData = PlayerScore(playerId,playerName,newScore)
-	#             playerData.save()
-	#             print('新增数据')
-	#             resultMsg="新增角色成功"
-	#         else:
-	#             tempStr=tempStr.replace("'",'"')
-	#             print('存在!%s' % tempStr)
-	#             # 如果存在就反序列化JSON
-	#             tempJson=json.loads(tempStr)
-	#             tempScore=int(tempJson['score'])
-	#             print('新分数是%d 原始分数是%d' % (newScore,tempScore))
-	#             # 然后比较分数，分数大则更新数据
-	#             if newScore > tempScore :
-	#                 playerData = PlayerScore(playerId, playerName, newScore)
-	#                 playerData.save()
-	#                 resultMsg = "更新分数记录成功%s" % playerName
-	#                 print("更新分数记录成功%s" % playerName)
-	#             else :
-	#                 resultMsg = "没有取得更好的分数%s" % playerName
-	#                 print("没有取得更好的分数%s" % playerName)
-	#     else:
-	#         # 如果密码错误就返回警告信息
-	#         resultMsg = 'password error'
-	#         print('password error')
-
 	if request.method == 'POST':
 		playerId = int(request.form.get('pid'))
 		playerName = request.form.get('pname')
@@ -80,39 +42,6 @@ def bbqRoute(model_type,getId,getCount):
 			# 如果密码错误就返回警告信息
 			resultMsg = 'password error'
 			print('password error')
-	# else:
-	# 	playerId = request.args.get('pid')
-	# 	playerName = request.args.get('pname')
-	# 	newScore = request.args.get('score')
-	# 	password = request.args.get('passwd')
-	# 	# 首先验证数据合法性
-	# 	if password:
-	# 		print('password is :%s' % password)
-	# 		# 如果是GET的话，就返回排行榜前10名
-	# 		count = 10
-	# 		tempC = PlayerScore.getTopGroup('score', -1, count)
-	# 		# 通过迭代将前10名的数据
-	# 		topArray = []
-	# 		for i in tempC:
-	# 			topArray.append(str(i))
-	# 		# 然后返回该账号当前排名
-	# 		curRank = PlayerScore.rankIndex('pname', playerName)
-	#
-	# 		reDice = {}
-	# 		reDice['count'] = count
-	# 		reDice['rank'] = curRank
-	# 		reDice['top'] = topArray
-	#
-	# 		resultMsg = json.dumps(reDice)
-	# 		return resultMsg
-	# 	else:
-	# 		# 如果密码错误就返回警告信息
-	# 		resultMsg = 'password error'
-	# 		print('password error')
-	#
-	# 	print('GET someth')
-	#
-	# 	return "result:%s %d" % (playerName, newScore)
 	else:
 		resultMsg= getCurRank(getId,getCount)
 
@@ -121,6 +50,10 @@ def bbqRoute(model_type,getId,getCount):
 
 def getCurRank(playerId,getCount):
 	# 首先验证数据合法性
+	if type(playerId) !=int or  type(getCount) != int :
+		print("参数类型错误")
+		return "error"
+
 	# 如果是GET的话，就返回排行榜前10名
 	count = getCount
 	tempC = PlayerScore.getTopGroup('score', -1, count)
